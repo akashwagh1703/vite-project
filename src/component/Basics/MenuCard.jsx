c40ec41ea7c93df933367acd0db911a36796e2a6
Akash Wagh <Akash.Wagh@esds.co.in>
2023-09-04 11:56:22 +0530

    menucard changes

--- src/component/Basics/MenuCard.jsx
+++ src/component/Basics/MenuCard.jsx
@@ -1,22 +1,26 @@
 // import React from 'react'
-
+import Menu from "./menuApi";
 
 const MenuCard = () => {
+
+  // console.log(Menu);
 
   return (
     <>
       <div className="card-container">
+      {Menu.map(data => (
         <div className="card">
           <div className="card-body">
-            <span className="card-number card-circle subtle">1</span>
-            <span className="card-author subtle">Breakfast</span>
-            <h2 className="card-title subtle">Maggi</h2>
-            <span className="card-description subtle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eligendi iste, incidunt deserunt inventore reprehenderit? Esse nisi eaque ut, ratione hic similique atque, porro iste perspiciatis ducimus, officiis possimus in.</span>
+            <span className="card-number card-circle subtle">{data.id}</span>
+            <span className="card-author subtle">{data.category}</span>
+            <h2 className="card-title subtle">{data.name}</h2>
+            <span className="card-description subtle">{data.description}</span>
             <div className="card-read">Read</div>
           </div>
-          {/* <img src={image} alt="image" className="card-media" /> */}
+          <img src={data.image} alt="image" className="card-media" />
           <span className="card-tag subtle">Order Now</span>
         </div>
+        ))};
       </div>
     </>
   )
